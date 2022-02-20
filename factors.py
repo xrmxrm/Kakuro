c4a8d2d74f4d3c391fde1b7d16a0dfb6cb8bd0dd
xrmxrm <xrmxrm@gmail.com>
2022-02-19 21:12:17 -0800

    Add max_combos

--- factors.py
+++ factors.py
@@ -37,27 +37,27 @@ def get_combos(desired_sum, length_of_word):
             combos.append(c)
     return combos
 
-def list_all():
+def list_all(max_combos=5):
     for desired_sum in range(3, max_sum + 1):
         print(desired_sum)
         for length_of_word in range(2, max_word + 1):
             combos = get_combos(desired_sum, length_of_word)
-            if combos and len(combos) < 5:
+            if combos and len(combos) < max_combos:
                 print('  ', length_of_word, combos)
             else:
-                if len(combos) >= 5:
+                if len(combos) >= max_combos:
                     print('  ', length_of_word, 'Too many to list')
             
 
 if __name__ == '__main__':
-    print('\nExpect:\n[(7, 8, 9)]\n')
-    print('Result:\n', get_combos(24, 3), sep='')
+    # print('\nExpect:\n[(7, 8, 9)]\n')
+    # print('Result:\n', get_combos(24, 3), sep='')
 
-    print('\nExpect:\n[(4, 7, 8, 9), (5, 6, 8, 9)]\n')
-    print('Result:\n', get_combos(28, 4), sep='')
+    # print('\nExpect:\n[(4, 7, 8, 9), (5, 6, 8, 9)]\n')
+    # print('Result:\n', get_combos(28, 4), sep='')
 
-    print('\nExpect:\n[]\n')
-    print('Result:\n', get_combos(45, 4), sep='')
+    # print('\nExpect:\n[]\n')
+    # print('Result:\n', get_combos(45, 4), sep='')
 
-    list_all()
+    list_all(20)
 
